4fc999d0144832cd5a8a63a89c793a8e0d14acb6
harshitajain828 <harshitajain828@gmail.com>
2023-11-01 22:06:57 +0530

    first commit

---
+++ app/components/Avatar.tsx
@@ -0,0 +1,29 @@
+'use client';
+
+import Image from "next/image";
+
+
+interface AvatarProps {
+    src: string | null | undefined;
+};
+
+
+import React from "react";
+
+const Avatar: React.FC<AvatarProps> = ({
+    src
+}) => {
+    return(
+        <Image 
+            className="rounded-full"
+            height="30"
+            width="30"
+            alt="Avatar"
+            src={src || "/images/placeholder.jpg"}
+        />
+
+    )
+}
+
+
+export default Avatar;
---
+++ next.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    experimental: {
+      
+      forceSwcTransforms: true
+    },
+    images: {
+      domains: [
+        "avatars.githubusercontent.com",
+        "lh3.googleusercontent.com",
+        "res.cloudinary.com"
+      ]
+    }
+  };
+  
